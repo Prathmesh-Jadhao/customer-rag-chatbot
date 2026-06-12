46be260b088569bdcb9f17093872174473a8d451
Prathamesh Jadhao <109366383+Prathmesh-Jadhao@users.noreply.github.com>
2026-06-13 03:31:18 +0530

    add Ollama fallback after OpenAI API failures due to key/config issues

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -5,14 +5,30 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
 def get_chatbot_chain(retriever):
-    llm = ChatOpenAI(model="gpt-4o")
+    llm = ChatOllama(
+    model="llama3.2",
+    temperature=0,
+    num_ctx=4096
+)
     
     prompt = ChatPromptTemplate.from_template("""
-    Answer the user's question based only on the following context:
-    {context}
-    
-    Question: {input}
-    """)
+You are a friendly customer support assistant for ABC Technologies Pvt. Ltd.
+
+Use the provided context to answer questions.
+
+If the user's message is a greeting (such as "Hi", "Hello", "Hey"), respond naturally and politely.
+
+If the answer is not present in the context, say:
+"I couldn't find that information in the knowledge base."
+
+Context:
+{context}
+
+Question:
+{input}
+
+Answer:
+""")
     
     # This creates the logic to combine retrieved docs with the LLM
     combine_docs_chain = create_stuff_documents_chain(llm, prompt)
